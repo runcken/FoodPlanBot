953868fae57ccc5d644648db7fe057ce339e0c44
VladislavVenin <d4spda@yandex.ru>
2025-10-23 06:28:58 +1100

    Добавил функцию возвращающую  случайное блюдо с учётом настроек пользователя

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,4 +1,16 @@
-from datacenter.models import Dish
+from datacenter.models import Dish, User
+import random
+
+
+def get_dish(chat_id):
+    user = User.objects.get(chat_id=chat_id)
+    filtered_dishes = Dish.objects.filter(
+        gluten_free=user.gluten_free,
+        vegan=user.vegan,
+        eco=user.eco,
+        price_lte=user.price,
+    )
+    return random.choice(filtered_dishes)
 
 
 def run():
