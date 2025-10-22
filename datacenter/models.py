b40eb5716335c324734bc36e5439c8513bddc34a
VladislavVenin <d4spda@yandex.ru>
2025-10-23 05:36:06 +1100

    Внёс дополнительные поля для настроек пользователя

--- datacenter/models.py
+++ datacenter/models.py
@@ -4,23 +4,27 @@ from django.db import models
 
 
 class Dish(models.Model):
-    title = models.CharField(("Название блюда"), max_length=50)
+    title = models.CharField("Название блюда", max_length=50)
     description = models.TextField("Описание", blank=True, default="")
-    image = models.ImageField(("Изображение"), blank=True, null=True)
+    image = models.ImageField("Изображение", blank=True, null=True)
     ingredients = models.TextField("Продукты")
     recipe = models.TextField("Рецепт")
-    gluten_free = models.BooleanField(("Без глютена"), default=False)
-    vegan = models.BooleanField(("Веганское"), default=False)
-    eco = models.BooleanField(("ЭКО"), default=False)
-    price = models.IntegerField(("Цена"))
+    gluten_free = models.BooleanField("Без глютена", default=False)
+    vegan = models.BooleanField("Веганское", default=False)
+    eco = models.BooleanField("ЭКО", default=False)
+    price = models.PositiveIntegerField("Цена")
 
     def __str__(self):
         return self.title
 
 
 class User(models.Model):
-    user_name = models.CharField(("Имя пользователя"), max_length=50)
+    user_name = models.CharField("Имя пользователя", max_length=50)
     chat_id = models.CharField(max_length=50)
+    gluten_free = models.BooleanField("Без глютена", default=False)
+    vegan = models.BooleanField("Веганское", default=False)
+    eco = models.BooleanField("ЭКО", default=False)
+    price = models.PositiveIntegerField("Цена", default=2147483647)
 
     def __str__(self):
         return self.user_name
