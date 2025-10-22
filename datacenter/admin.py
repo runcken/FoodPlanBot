553b26d0b8dfbc35110579d35d6bb4c1c1deba75
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-10-23 01:07:37 +0300

    Настроил фильтры и интерфейс для админки

--- datacenter/admin.py
+++ datacenter/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Dish, User
 
-# Register your models here.
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ['title', 'gluten_free', 'vegan', 'eco' , 'price']
+    list_filter = ['gluten_free', 'vegan', 'eco']
+    search_fields = ['title', 'gluten_free', 'vegan', 'eco', 'description']
 
-admin.site.register(Dish)
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'chat_id', 'gluten_free', 'vegan', 'eco', 'price' ]
+    list_filter = ['gluten_free', 'vegan', 'eco']
+    search_fields = ['user_name', 'gluten_free', 'vegan', 'eco', 'chat_id']
