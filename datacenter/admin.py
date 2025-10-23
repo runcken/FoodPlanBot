be740bce3598b4132490fbb4daba831360dfcf07
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-10-23 22:21:33 +0300

    Добавил новую модель в админку

--- datacenter/admin.py
+++ datacenter/admin.py
@@ -1,11 +1,28 @@
 from django.contrib import admin
-from .models import Dish, User
+from .models import Dish, User, DishProduct,Product
+
+class DishProductInline(admin.TabularInline):
+    model = DishProduct
+    extra = 1
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    search_fields = ['name']
 
 @admin.register(Dish)
 class DishAdmin(admin.ModelAdmin):
-    list_display = ['title', 'gluten_free', 'vegan', 'eco' , 'price']
-    list_filter = ['gluten_free', 'vegan', 'eco']
-    search_fields = ['title', 'gluten_free', 'vegan', 'eco', 'description']
+    list_display = ['title', 'price', 'gluten_free', 'vegan', 'eco']
+    list_filter = ['gluten_free', 'vegan', 'eco',]
+    search_fields = ['title', 'description',]
+    inlines = (DishProductInline,)
+
+    def save_related(self, request, form, formsets, change):
+        super().save_related(request, form, formsets, change)
+        dish = form.instance
+        total = sum(dp.price for dp in dish.product_dish.all())
+        dish.price = total
+        dish.save(update_fields=['price'])
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
