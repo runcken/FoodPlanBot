ecbf44994e078467dab14852bfb2d4b6eb4fa7a9
Chop-Chopa <136185471+Chop-Chopa@users.noreply.github.com>
2025-10-23 22:19:03 +0300

    Обновил модели для добавление продуктов в блюдо

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 
-# Create your models here.
-
-
 class Dish(models.Model):
     title = models.CharField("Название блюда", max_length=100)
     description = models.TextField("Описание", blank=True, default="")
     image = models.ImageField("Изображение", blank=True, null=True)
-    ingredients = models.TextField("Продукты")
-    recipe = models.TextField("Рецепт")
+    ingredients = models.TextField("Продукты", blank=True, null=True)
     gluten_free = models.BooleanField("Без глютена", default=False)
     vegan = models.BooleanField("Веганское", default=False)
     eco = models.BooleanField("ЭКО", default=False)
-    price = models.PositiveIntegerField("Цена")
+    recipe = models.TextField("Рецепт")
+    price = models.PositiveIntegerField("Цена", default=0)  # Цена блюда
 
     class Meta:
         ordering = ('title',)
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
 
     def __str__(self):
         return self.title
@@ -38,3 +35,45 @@ class User(models.Model):
 
     def __str__(self):
         return self.user_name
+
+
+class Product(models.Model):
+    name = models.CharField("Название продукта", max_length=100)
+    price = models.PositiveIntegerField("Цена за 100 грамм", default=0)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
+
+
+class DishProduct(models.Model):
+    dish = models.ForeignKey(
+        Dish,
+        on_delete=models.CASCADE,
+        related_name='product_dish',
+        verbose_name='Блюдо',
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name='Продукт'
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Масса в граммах')
+
+    class Meta:
+        ordering = ('dish',)
+        verbose_name = 'Продукт и цена'
+        verbose_name_plural = 'Продукты и цена'
+
+    def __str__(self):
+        return f'{self.dish.title} - {self.product.name}'
+
+    @property
+    def price(self):
+        """Цена ингредиента по массе"""
+        return self.product.price * self.quantity // 100
+
